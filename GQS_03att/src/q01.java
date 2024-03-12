d997c9da42cc89c952397b03e56da8672bc04d6d
Paulo Henrique Domingos <90566724+pyhpaulo@users.noreply.github.com>
2024-03-11 22:14:54 -0300

    Add files via upload

---
+++ GQS_03att/src/q01.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class q01 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Ler a idade e o sexo do cliente
+        System.out.print("Digite a sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.print("Digite o seu sexo (M/F): ");
+        String sexo = scanner.next();
+
+        // Calculando o valor da mensalidade
+        double valorMensalidade = 0.0;
+
+        if (sexo.equals("M")) {
+            if (idade <= 15) {
+                valorMensalidade = 60.0;
+            } else if (idade <= 18) {
+                valorMensalidade = 75.0;
+            } else if (idade <= 30) {
+                valorMensalidade = 90.0;
+            } else if (idade <= 40) {
+                valorMensalidade = 85.0;
+            } else {
+                valorMensalidade = 80.0;
+            }
+        } else if (sexo.equals("F")) {
+            if (idade <= 18) {
+                valorMensalidade = 60.0;
+            } else if (idade <= 25) {
+                valorMensalidade = 90.0;
+            } else if (idade <= 40) {
+                valorMensalidade = 85.0;
+            } else {
+                valorMensalidade = 80.0;
+            }
+        }
+
+        // Resultado do valor da mensalidade
+        System.out.println("O valor da sua mensalidade é R$" + valorMensalidade);
+    }
+}
+
